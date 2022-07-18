a55f6b2bc27008309e4eba775b156bd7800eecb6
LuisHidalgoAguilar <lha.mango@gmail.com>
2022-07-18 18:09:42 +0200

    Custom Validators (CustomValidator)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,7 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 export class AppComponent implements OnInit {
 
     genders = ['male', 'female'];
+    forbiddenNames = ["Pepe", "Manolo"];
     signUpForm: FormGroup;
 
 
@@ -16,7 +17,9 @@ export class AppComponent implements OnInit {
         this.signUpForm = new FormGroup({
             //Subgrupo
             "userData": new FormGroup({
-                "username": new FormControl(null, [Validators.required, Validators.minLength(5)]),
+                //CustomValidator
+                //Es importante hacer el "bind(this)" para que el "this" sea el correcto.
+                "username": new FormControl(null, [Validators.required, Validators.minLength(3), this.forbiddenNamesValidator.bind(this)]),
                 "email": new FormControl(null, [Validators.required, Validators.email]),
             }),
             "gender": new FormControl("female")
@@ -25,6 +28,15 @@ export class AppComponent implements OnInit {
 
     onSubmit() {
         console.log(this.signUpForm.value);
+    }
+
+    //CustomValidator
+    //Si el control pasa la validación hay que devolver "null"
+    forbiddenNamesValidator(control: FormControl): { [s: string]: boolean } {
+        if (this.forbiddenNames.indexOf(control.value) !== -1) {
+            return { "isForbiddenName": true };
+        }
+        return null;
     }
 
 }
